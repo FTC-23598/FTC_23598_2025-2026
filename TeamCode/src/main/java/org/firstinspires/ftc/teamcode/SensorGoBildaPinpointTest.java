13154236ab268dd0fed4dbe5094caa43c9806cc6
Phil Szymanowski <pszymanow@gmail.com>
2025-11-18 20:18:03 -0500

    Changed offset and units of pinpoint sensor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorGoBildaPinpointTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorGoBildaPinpointTest.java
@@ -25,6 +25,7 @@ import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.sun.tools.javac.comp.Todo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -84,7 +85,7 @@ public class SensorGoBildaPinpointTest extends OpMode {
         *  The Y pod offset refers to how far forwards from the tracking point the Y (strafe) odometry pod is.
         *  Forward of center is a positive number, backwards is a negative number.
         */
-        pinpoint.setOffsets(-84.0, -168.0, DistanceUnit.MM); //these are tuned for 3110-0002-0001 Product Insight #1
+        pinpoint.setOffsets(45, 71, DistanceUnit.MM); //these are tuned for 3110-0002-0001 Product Insight #1
 
         /*
          * Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
